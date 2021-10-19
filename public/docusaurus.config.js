ae26c6fbd25a3a8b52e04bfd3f2a83ce8ce8c593
OSCA-Ado-Ekiti <oscaadoekiti@gmail.com>
2021-10-19 08:35:57 +0100

    Update docusaurus.config.js

--- public/docusaurus.config.js
+++ public/docusaurus.config.js
@@ -92,7 +92,7 @@ module.exports = {
           routeBasePath: '/',
           sidebarPath: require.resolve('./sidebars.js'),
           editUrl:
-            'https://github.com/OSCA-Ado-Ekiti/Hacktoberfest2021-Ekiti/edit/main/public/docs',
+            'https://github.com/OSCA-Ado-Ekiti/Hacktoberfest2021-Ekiti/edit/main/public',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
